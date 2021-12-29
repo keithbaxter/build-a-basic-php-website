104401c82883089727aeddd148d256ca160324e0
Keith Baxter <keith.baxter@gmail.com>
2021-12-29 15:03:56 -0500

    Add details for each category and update links

--- inc/functions.php
+++ inc/functions.php
@@ -1,7 +1,8 @@
 <?php
 
     function get_item_html($id, $item) {
-        $output = "<li><a href='#'><img src='"
+        $output = "<li><a href='details.php?id="
+            . $id . "'><img src='"
             . $item["img"] . "' alt='"
             . $item["title"] . "' />"
             . "<p>View Details</p>" 
@@ -13,7 +14,7 @@
         $output = [];
 
         foreach($catalog as $id => $item) {
-            if($category == null OR strtolower($category) == strtolower($item["category"])) {
+            if($category == null || strtolower($category) == strtolower($item["category"])) {
                 $sort = $item["title"];
                 $sort = ltrim($sort, "The ");
                 $sort = ltrim($sort, "A ");
